a88d0cdca1b14e310dc92d386aaeb70532d1cc4c
Juuso Ansaharju <juuso.ansaharju@gmail.com>
2013-09-15 23:35:18 +0300

    Layout & text toggles added

--- assignments/AssignmentsSolution/UiConstruction/MainPage.xaml.cs
+++ assignments/AssignmentsSolution/UiConstruction/MainPage.xaml.cs
@@ -20,5 +20,47 @@ namespace UiConstruction
         {
             InitializeComponent();
         }
+
+        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void explanationToggle_Click(object sender, RoutedEventArgs e)
+        {
+            // Toggle explanations
+            if (publicExplanation.Visibility == Visibility.Collapsed && privateExplanation.Visibility == Visibility.Collapsed)
+            {
+                publicExplanation.Visibility = Visibility.Visible;
+                privateExplanation.Visibility = Visibility.Visible;
+                explanationToggle.Content = "Hide explanation";
+            }
+            else
+            {
+                publicExplanation.Visibility = Visibility.Collapsed;
+                privateExplanation.Visibility = Visibility.Collapsed;
+                explanationToggle.Content = "Show explanation";
+            }
+        }
+
+        private void privateRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            privateWarningBlock.Visibility = Visibility.Visible;
+        }
+
+        private void privateRadio_Unchecked(object sender, RoutedEventArgs e)
+        {
+            privateWarningBlock.Visibility = Visibility.Collapsed;
+        }
+
+        private void lightCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            lightNoteBlock.Visibility = Visibility.Visible;
+        }
+
+        private void lightCheck_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lightNoteBlock.Visibility = Visibility.Collapsed;
+        }
     }
 }
