d7769794985929ad62142351df26a5a237661620
Juuso Ansaharju <juuso.ansaharju@gmail.com>
2013-09-19 00:01:12 +0300

    Added localization system

---
+++ assignments/AssignmentsSolution/UiConstruction/LocalizedStrings.cs
@@ -0,0 +1,18 @@
+﻿namespace UiConstruction
+{
+    public class LocalizedStrings
+    {
+        /// <summary>
+        /// Note that we do not need to specify the namespace when creating the instance
+        /// </summary>
+        private static L10N localizedResources = new L10N();
+
+        /// <summary>
+        /// Gets of AppResources class instance
+        /// </summary>
+        public L10N L10N
+        {
+            get { return localizedResources; }
+        }
+    }
+}
