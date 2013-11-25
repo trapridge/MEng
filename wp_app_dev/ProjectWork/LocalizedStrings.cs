8dd16d8c6c67e925c4f7bf2357a8879dc4f7431c
Juuso Ansaharju <juuso.ansaharju@gmail.com>
2013-11-26 01:00:38 +0200

    added l10n

---
+++ wp_app_dev/ProjectWork/LocalizedStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectWork
+{
+    public class LocalizedStrings
+    {
+        /// <summary>
+        /// Note that we do not need to specify the namespace when creating the instance
+        /// </summary>
+        private static L10N localizedResources = new L10N();
+
+        /// <summary>
+        /// Gets of AppResources class instance
+        /// </summary>
+        public L10N L10N
+        {
+            get { return localizedResources; }
+        }
+    }
+}
+
