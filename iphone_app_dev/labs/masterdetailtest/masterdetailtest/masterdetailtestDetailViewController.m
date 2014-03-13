9eaf52a4c25206c649e53f6a781b877d3f936c7c
Juuso Ansaharju <juuso.ansaharju@gmail.com>
2014-03-13 17:44:34 +0200

    Added a test project

---
+++ iphone_app_dev/labs/masterdetailtest/masterdetailtest/masterdetailtestDetailViewController.m
@@ -0,0 +1,51 @@
+//
+//  masterdetailtestDetailViewController.m
+//  masterdetailtest
+//
+//  Created by jansaharju on 27/02/14.
+//  Copyright (c) 2014 m0000892. All rights reserved.
+//
+
+#import "masterdetailtestDetailViewController.h"
+
+@interface masterdetailtestDetailViewController ()
+- (void)configureView;
+@end
+
+@implementation masterdetailtestDetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iphone_app_dev/labs/masterdetailtest/masterdetailtest/masterdetailtestMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  masterdetailtestMasterViewController.h
+//  masterdetailtest
+//
+//  Created by jansaharju on 27/02/14.
+//  Copyright (c) 2014 m0000892. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface masterdetailtestMasterViewController : UITableViewController
+
+@end
